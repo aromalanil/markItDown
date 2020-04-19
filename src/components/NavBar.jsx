d4d23f335c1d86f8eb55c19d644771bcd064c5db
Aromal Anil <aromalanilkannan@gmail.com>
2020-04-20 01:55:25 +0530

    Removed unwanted code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import GitHub from '../icons/github.svg'
 function NavBar() {
   return (
     <nav className="navbar">
-        <h3 onClick={()=>window.open(window.location.hostname)} className="title"><img src={MarkDownLogo} alt=""/>&nbsp;Markdown Editor</h3>
+        <h3 className="title"><img src={MarkDownLogo} alt=""/>&nbsp;Markdown Editor</h3>
         <img onClick={()=>window.open('https://github.com/aromalanil/Markdown-Editor')}  className="github" src={GitHub} alt=""/>
     </nav>
   );
