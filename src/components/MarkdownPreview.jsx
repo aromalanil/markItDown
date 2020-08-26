40716b6e56dcae19272a9847ccaf9a30f2e1cdda
Aromal Anil <aromalanilkannan@gmail.com>
2020-08-26 09:18:49 +0530

    🍫 Added Save Option

--- src/components/MarkdownPreview.jsx
+++ src/components/MarkdownPreview.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import * as marked from 'marked';
-
+import * as marked from "marked";
 
 function MarkdownPreview({ content }) {
   const [html, setHtml] = useState(getHtml(content));
@@ -9,23 +8,18 @@ function MarkdownPreview({ content }) {
     setHtml(getHtml(content));
   }, [content]);
 
-  const handleSaveButton=()=>{
-    
-  }
-
   return (
     <div className="markdown-preview scroll">
       <div className="section-title">
         <h3>Preview</h3>
-        <button onClick={handleSaveButton} className="btn">
-            Save
-          </button>
+        <a href={`data:text/html,${html}`} download="export.html" className="btn">
+          Save
+        </a>
       </div>
       <div
         id="preview"
         className="html-div"
-        dangerouslySetInnerHTML={{ __html: html }}
-      ></div>
+        dangerouslySetInnerHTML={{ __html: html }}></div>
     </div>
   );
 }
