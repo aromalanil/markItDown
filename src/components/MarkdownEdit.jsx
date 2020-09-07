a8e5d88a8df85d0c95860744a56d0a22ca71db1b
Aromal Anil <aromalanilkannan@gmail.com>
2020-09-07 11:06:04 +0530

    📺 Added Fullscreen Preview

--- src/components/MarkdownEdit.jsx
+++ src/components/MarkdownEdit.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useState,useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Snackbar from "@material-ui/core/Snackbar";
 import { Alert, AlertTitle } from "@material-ui/lab";
 import placeholder from "../data/placeholder";
 
 function MarkdownEdit({ content, changeContent }) {
   const [open, setOpen] = useState(false);
-  const editorRef =useRef(null);
+  const editorRef = useRef(null);
 
   useEffect(() => {
-    if(content===''){
-      localStorage.setItem("markdown",placeholder);
+    if (content === "") {
+      localStorage.setItem("markdown", placeholder);
+    } else {
+      localStorage.setItem("markdown", content);
     }
-    else{
-      localStorage.setItem("markdown",content);
-    }
-  }, [content])
+  }, [content]);
 
   const handleEditorChange = (event) => {
     event.preventDefault();
@@ -41,7 +40,7 @@ function MarkdownEdit({ content, changeContent }) {
         <h3>Markdown</h3>
         <div className="right-section">
           <button onClick={handleCopyButton} className="btn">
-            Copy
+          Copy
           </button>
           <button onClick={handleClearButton} className="btn">
             Clear
@@ -53,16 +52,14 @@ function MarkdownEdit({ content, changeContent }) {
         value={content}
         onChange={handleEditorChange}
         id="editor"
-        ref={editorRef}
-      ></textarea>
+        ref={editorRef}></textarea>
 
       <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
         <Alert
           onClose={handleClose}
           severity="success"
           elevation={6}
-          variant="filled"
-        >
+          variant="filled">
           <AlertTitle>Copied</AlertTitle>
           The markdown is copied to your clipboard
         </Alert>
--- src/components/MarkdownPreview.jsx
+++ src/components/MarkdownPreview.jsx
@@ -1,32 +1,51 @@
 import React, { useState, useEffect } from "react";
 import * as marked from "marked";
-import Prism from 'prismjs';
-import '../utils/prism-imports';
+import Prism from "prismjs";
+import "../utils/prism-imports";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 //css for Prism is imported in ThemeSelector
 
 function MarkdownPreview({ content }) {
   const [html, setHtml] = useState(getHtml(content));
+  const handle = useFullScreenHandle();
 
-  useEffect(()=>{
+  useEffect(() => {
     Prism.highlightAll();
-  })
+  });
 
   useEffect(() => {
     setHtml(getHtml(content));
   }, [content]);
 
+  const handleFullScreen = () =>
+    handle.active ? handle.exit() : handle.enter();
+
+  const handleSaveClick = () => {
+    let link = document.createElement("a");
+    link.href=`data:text/html,${html}`;
+    link.download="export.html";
+    link.click();
+  };
+
   return (
     <div className="markdown-preview scroll">
       <div className="section-title">
         <h3>Preview</h3>
-        <a href={`data:text/html,${html}`} download="export.html" className="btn">
-          Save
-        </a>
+        <div className="right-section">
+          <button className="btn" onClick={handleSaveClick}>
+            Save
+          </button>
+          <button className="btn" onClick={handleFullScreen}>
+            Fullscreen
+          </button>
+        </div>
       </div>
-      <div
-        id="preview"
-        className="html-div"
-        dangerouslySetInnerHTML={{ __html: html }}></div>
+      <FullScreen handle={handle}>
+        <div
+          id="preview"
+          className={`html-div ${handle.active ? "preview-fullscreen" : ""}`}
+          dangerouslySetInnerHTML={{ __html: html }}></div>
+      </FullScreen>
     </div>
   );
 }
